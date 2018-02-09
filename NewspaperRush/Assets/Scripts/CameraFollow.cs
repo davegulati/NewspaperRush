a1612afa0632bd7ba3f6e6751953f8522fbde9f1
Dave Gulati <1006106@student.sae.edu.au>
2018-02-09 18:42:49 +1100

    Commit message number 1.1 in dev log.

---
+++ NewspaperRush/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform player;
+
+    public float smoothSpeed = 10.0f;
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        if (GameObject.Find("Player") == null)
+        {
+            Debug.LogError("No 'Player' gameobject found in the scene!");
+        }
+        else
+        {
+            player = GameObject.Find("Player").transform;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
+    }
+}
---
+++ NewspaperRush/Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private ConstantForce cF;
+    private float jumpForce = 400.0f;
+
+    public float playerMoveForwardSpeed = 5.0f;
+
+    private void Awake()
+    {
+        if (GetComponent<Rigidbody>() == null)
+        {
+            Debug.LogError("No Rigidbody component found on player!");
+        }
+        else
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        if (GetComponent<ConstantForce>() == null)
+        {
+            Debug.LogError("No Constant Force component found on player!");
+        }
+        else
+        {
+            cF = GetComponent<ConstantForce>();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        cF.force = Vector3.forward * playerMoveForwardSpeed;
+    }
+}
